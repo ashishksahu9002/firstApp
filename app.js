42aa3233444b09bad951e726b8cdbb1db37f64f8
ashishksahu9002 <ashishksahu9002@gmail.com>
2020-12-24 13:39:31 +0530

    Adding my firstApp to github

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello server")
+})
+
+const PORT = 5000
+
+app.listen(PORT,()=>{
+    console.log('Server is running')
+})
